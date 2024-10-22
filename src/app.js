de347d5d4a39fc7fda5f427e74c99098c3b9b546
Marat <amr404@bk.ru>
2024-10-22 15:39:14 +0300

    update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 const $circle = document.querySelector('#circle');
 const $score = document.querySelector('#score');
+const btn = document.querySelector('#btn');
+const nextLevel = document.querySelector('#nextLevel');
+
+let reset = () => {
+  if(confirm('Are you sure?')) setScore(0);
+  $circle.src = './assets/frog.png';
+  nextLevel.textContent = '✅ next level 50 coins';
+};
+
+btn.addEventListener('click', reset);
 
 function start() {
   setScore(getScore());
-  setImage()
+  setImage();
 }
 
 function setScore(score) {
@@ -20,9 +30,22 @@ function addOne() {
   setImage()
 }
 
+function showAlert() {
+  const alertBox = document.querySelector('#alert');
+  alertBox.style.display = 'block'; // Показать уведомление
+
+  setTimeout(() => {
+    alertBox.style.display = 'none'; // Скрыть уведомление
+  }, 1000);
+}
+
 function setImage() {
+  if(getScore() === 50) {
+    showAlert();
+  }
   if(getScore() >= 50) {
     $circle.src = './assets/lizzard.png';
+    nextLevel.textContent = '✅';
   }
 }
 
@@ -37,6 +60,7 @@ $circle.addEventListener('click', e => {
 
   $circle.style.setProperty('--tiltX', `${tiltX}deg`);
   $circle.style.setProperty('--tiltY', `${tiltY}deg`);
+
   setTimeout(() => {
     $circle.style.setProperty('--tiltX', `0deg`);
     $circle.style.setProperty('--tiltY', `0deg`);
